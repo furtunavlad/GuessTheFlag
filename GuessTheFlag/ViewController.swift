339d3d77e88c213634cd57eeffd62eca3f6c12bf
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2024-02-17 23:56:52 +0200

    initial commit

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var buttonTop: UIButton!
+    @IBOutlet var buttonMiddle: UIButton!
+    @IBOutlet var buttonBottom: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        askQuestion()
     }
-
+    
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        buttonTop.setImage(UIImage(named: countries[0]), for: .normal)
+        buttonMiddle.setImage(UIImage(named: countries[1]), for: .normal)
+        buttonBottom.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
+    }
+    
+    
+    @IBAction func ButtonTapped(_ sender: UIButton) {
+        
+    }
+    
 
 }
 
